c53f3a6169f1108685d97acdf81171ef578a3084
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2024-06-28 21:18:00 +0530

    oip

---
+++ old/1.0.4_0 (M2) [FireFox] - Copy/scripts/service.js
@@ -0,0 +1,70 @@
+browser.runtime.onStartup.addListener(async () => {
+  var a = new Promise(function(resolve, reject){
+        browser.storage.local.get({"enabled": true}, function(options){
+            resolve(options.enabled);
+      })
+  });
+
+  const enabled = await a;
+  console.log(enabled);
+  if (enabled) {
+    await enable();
+  } else {
+    await disable();
+  }
+});
+
+browser.storage.onChanged.addListener(async (changes, namespace) => {
+  if (namespace !== "local") return;
+
+  if (changes.enabled) {
+    if (changes.enabled.newValue) {
+      await enable();
+    } else {
+      await disable();
+    }
+  }
+
+});
+
+async function enable() {
+  browser.browserAction.setIcon({
+    path: {
+      32: "images/icon-32.png",
+      38: "images/icon-38.png",
+      128: "images/icon-128.png",
+    },
+  });
+  await reloadAffectedTab();
+}
+
+/**
+ * @returns Promise
+ */
+async function disable() {
+  browser.browserAction.setIcon({
+    path: {
+      32: "images/icon-disabled-32.png",
+      38: "images/icon-disabled-38.png",
+      128: "images/icon-disabled-128.png",
+    },
+  });
+  await reloadAffectedTab();
+}
+
+/**
+ * @returns Promise
+ */
+
+
+async function reloadAffectedTab() {
+  const [currentTab] = await browser.tabs.query({
+    active: true,
+    url: "*://*.youtube.com/*",
+  });
+  const isTabAffected = Boolean(currentTab?.url);
+  if (isTabAffected) {
+    return browser.tabs.reload();
+  }
+}
+
